e6de2e4c2fe462f9002ac5d9580d878616837313
adnan2508 <adnandiu2800@gmail.com>
2024-06-22 00:49:08 +0600

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <>
+      <div className="navbar bg-primary">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Link</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
